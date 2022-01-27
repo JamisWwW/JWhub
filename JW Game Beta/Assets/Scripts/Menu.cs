7b11de636ba47f34289507d64c2705eb890d6236
TTaPeHe4eK <96486391+TTaPeHe4eK@users.noreply.github.com>
2022-01-28 01:29:02 +0300

    Main menu scene

---
+++ JW Game Beta/Assets/Scripts/Menu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+    public void ChooseScene(int a)                  
+    {
+        SceneManager.LoadScene(a);                  
+    }
+    public void Exit()                             
+    {
+        Application.Quit();                       
+    }
+}
+
